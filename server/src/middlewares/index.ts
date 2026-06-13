6d0d63caa51bba56b04ac9cf856be5db7acc1a2c
hunter87ff <hunter87.dev@gmail.com>
2026-06-13 23:58:46 +0530

    feat: integrate CORS and rate limiting, refactor UserToken to utility, and define new application models

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from 'express';
 import models from "@/models";
 import cookieParser from 'cookie-parser';
@@ -5,6 +6,9 @@ import validator from "@/validator";
 import { Sanitizer } from '@/utils/sanitize';
 import { ResponseHandler } from '@/utils/response';
 import { Logger } from '@/utils/logger';
+import Helper from '@/utils/helper';
+import RateLimit from "express-rate-limit";
+import type { NextFunction, Request, Response } from 'express';
 
 // import type { Token } from '@/utils/wrappers/token';
 
@@ -13,6 +17,7 @@ declare module 'express-serve-static-core' {
     interface Application {
         db: typeof models;
         // schema: typeof schema;
+        helper: Helper;
         logger: Logger;
         jwts: Set<string>;
         sanitize: Sanitizer;
@@ -21,6 +26,7 @@ declare module 'express-serve-static-core' {
     }
     interface Request {
         // user?: Token;
+        helper: Helper;
         fullUrl?: string;
         sanitize: Sanitizer;
         is_admin?: boolean;
@@ -37,6 +43,30 @@ declare module 'express-serve-static-core' {
     }
 }
 
+
+async function init(req: Request, res: Response, next: NextFunction){
+    req.helper = Helper;
+    req.sanitize = Sanitizer;
+    req.validator = validator;
+    res.logger = Logger.instance;
+    res.handler = req.app.responseHandler;
+    next();
+}
+
+
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later',
+});
+
+
+async function security(req : Request, res : Response, next: NextFunction){
+    limiter(req, res, next);
+}
+
+
+
 export default async function middlewares(app: express.Express) {
     app.use(express.json({ limit: '5MB' }));
     app.use(cookieParser());
@@ -45,12 +75,16 @@ export default async function middlewares(app: express.Express) {
     app.db = models;
     // app.schema = schema;
     // app.cache = cache;
-    // app.helper = helper;
+    app.helper = Helper;
     app.logger = Logger.instance;
     app.sanitize = Sanitizer;
     app.validator = validator;
     app.jwts = new Set<string>([]);
     app.responseHandler = new ResponseHandler();
+
+    app.use(init)
+
+    app.use(security)
 
     // app.use(initialMiddleware);
     // app.use(securityHeaders());
--- server/src/utils/helper.ts
+++ server/src/utils/helper.ts
@@ -1,64 +1,7 @@
 
 import config from "@/config";
-import {logger} from "./logger";
-import { Role } from '@/enums';
 import {hash, compare} from "bcryptjs";
 import {sign, verify, decode} from 'jsonwebtoken';
-
-export class UserToken{
-    id : string;
-    name : string;
-    role : {
-        admin : boolean;
-        manager : boolean;
-    };
-    token : string;
-    private __role : Role;
-
-    constructor({id, name, role} : {id : string, name : string, role : Role}){
-        this.id = id;
-        this.name = name;
-        this.__role = role;
-        this.role = {
-            admin : role === Role.ADMIN,
-            manager : role === Role.MANAGER
-        };
-        this.token = this.getToken();
-    }
-
-    toJson(){
-        return {
-            id : this.id,
-            name : this.name,
-            role : this.__role
-        }
-    }
-
-    getToken(){
-        if (this.token){
-            return this.token;
-        }
-        const token = sign(this.toJson(), config.jwt_secret, {expiresIn : config.jwt_expires});
-        this.token = token;
-        return token;
-    }
-
-    static async fromToken(token : string){
-        try{
-            const payload = verify(token, config.jwt_secret);
-            if(!payload) {return null;}
-            const {id, name, role} = payload as any;
-            if(!id || !name || !role) {return null;}
-            const userToken = new UserToken({id, name, role});
-            userToken.token = token;
-            return userToken;
-        }catch(err){
-            logger.error(err);
-            return null;
-        }
-    }
-
-}
 
 
 export default class Helper {
