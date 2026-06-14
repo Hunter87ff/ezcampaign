e541681411342eb21cab3a836e6f3b1efff7788c
hunter87ff <hunter87.dev@gmail.com>
2026-06-14 23:27:21 +0530

    added dev command

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -5,7 +5,9 @@ import {logger} from "@/utils/logger";
 export default async function db(){
     try {
         const uri = config.db_uri;
-        const con = await mongoose.connect(uri);
+        const con = await mongoose.connect(uri, {
+            dbName : "ezcampaign"
+        });
         logger.info("Database connected", con.connection.host);
         return con;
     } catch (error) {
